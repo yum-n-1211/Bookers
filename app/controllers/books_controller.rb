b829d3f0d589d2251b004a3744c11bb8a43cc656
EC2 Default User <ec2-user@ip-172-31-18-245.ec2.internal>
2022-01-21 00:18:00 +0000

    [Finish]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,20 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(book.id)
+    else
+      @books = Book.none
+      render :index
+    end
   end
 
   def index
-    @book =Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -22,19 +26,22 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books' 
+    redirect_to '/books'
   end
-  
+
   private
   # ストロングパラメータ
   def book_params
